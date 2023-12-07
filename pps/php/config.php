87739244245da92bbe736b59518cbc49119777b4
A. Muzacky Naufal Ammar <107091692+am-naufal@users.noreply.github.com>
2023-12-08 06:48:40 +0700

    Update config.php

--- pps/php/config.php
+++ pps/php/config.php
@@ -3,11 +3,13 @@ $dbhost ='localhost';
 $dbuser ='u121228870_pps';
 $dbpass ='ODv:V:^GbftL';
 $dbname ='u121228870_db_pps';
+$db_dsn = "mysql:dbname=$dbname;host=$dbhost";
 try {
   $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 } catch (mysqli_sql_exception $e) {
   echo 'Koneksi gagal: ' . $e->getMessage();
 }
+
 /*css.plugin.hancon <?php echo $base; ?>*/
 $base='https://lubabulhasanah.2020tiputri.com/pps';
 /*control(link.redirect) <?php echo $basecon; ?>*/
