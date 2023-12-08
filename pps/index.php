825fa6541e28ff3407644838870181613e2b1dd7
A. Muzacky Naufal Ammar <107091692+am-naufal@users.noreply.github.com>
2023-12-08 10:36:43 +0700

    Update index.php

--- pps/index.php
+++ pps/index.php
@@ -4,7 +4,7 @@ if(isset($_SESSION['c_admin'])){ header('location:admin/'); }
 elseif(isset($_SESSION['c_guru'])){ header('location:guru/'); }
 elseif(isset($_SESSION['c_orangtua'])){ header('location:walimurid/'); }
 // else{header('location:login');}
-else{header('location:maintence_page');}
+else{header('location:maintenance_page');}
 
 
 ?>
