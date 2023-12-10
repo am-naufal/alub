e801c09b8d78c2dca5fb3074b58cdce53b4c51c0
A. Muzacky Naufal Ammar <107091692+am-naufal@users.noreply.github.com>
2023-12-10 13:57:21 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <main>
+
+        <div class="container-fluid">
+            <div class="row p-5 justify-content-center align-items-center">
+                <div class="col-md-6 ">
+                    <div class="">
+                        <h1 class="text-center mt-5 mb-5 text-danger">SORRY!</h1>
+                        <p class="h2 text-primary text-center">Our website is under Construction</p>
+                        <p class="fs-3 text-center">kami sedang berusaha mencari tutorial untuk menyelesaikan masalah
+                            yang
+                            kami hadapi</p>
+                        <form action="" method="post">
+                            <div class="row mt-5">
+
+                                <input t ype="text" name="" id=""
+                                    class="rounded-5 p-2 bg-light-blue col-8 m-auto mb-3 ">
+                                <div class="d-grid gap-2">
+                                    <button type="button" name="" id=""
+                                        class="btn btn-primary rounded-5 p-2 col-8 m-auto fw-bolder">
+                                        Support saya
+                                    </button>
+                                </div>
+
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+                <div class="col-md-6 p-5">
+                    <img src="php/img/j.png" alt="" width="100%">
+                </div>
+            </div>
+        </div>
+
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
